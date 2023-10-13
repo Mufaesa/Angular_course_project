844bbf91b3cc6ec85edfc77aeecdb2693b030f53
Tom Vrijmoet <tom.vrijmoet@ns.nl>
2023-10-13 17:08:27 +0200

    Replaced overcomplicated recipe logic with service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,20 +9,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() sendRecipeDetailsToRecipes = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('Spaghet', 'Spaghetti and meatballs', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Patatje pinda', 'Patat & een pinda', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
-
-  sendDataToRecipesComponent(recipe: Recipe){
-    this.sendRecipeDetailsToRecipes.emit(recipe);
-  }
-
 }
